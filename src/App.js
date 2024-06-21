30d9acb849a7b1bfe1d7f69627341b0151737c85
hbruckman <hbruckman@bayamon.inter.edu>
2024-06-21 01:09:01 -0400

    Exercise 2: Sign Up and Login Components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 
 import LandingPage from './Components/LandingPage/LandingPage';
+import SignUp from './Components/SignUp/SignUp';
+import Login from './Components/Login/Login';
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -27,19 +27,19 @@ function Navbar() {
         </div>
         <ul className="nav__links active">
           <li className="link">
-            <a href="../Landing_Page/LandingPage.html">Home</a>
-          </li>
+            <a href="../LandingPage/LandingPage.html">Home</a>
+        </li>
           <li className="link">
             <a href="#">Appointments</a>
           </li>
 
           <li className="link">
-            <a href="../Sign_Up//Sign_Up.html">
+            <a href="../SignUp">
               <button className="btn1">Sign Up</button>
             </a>
           </li>
           <li className="link">
-            <a href="../Login/Login.html">
+            <a href="../Login">
               <button className="btn1">Login</button>
             </a>
           </li>
